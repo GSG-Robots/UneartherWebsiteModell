6e1bbec0fad2150034cbe448dee27b3d85f25139
JoJoJux (aka. J0J0HA) <johannes@jojojux.de>
2026-05-02 19:48:56 +0200

    Make sidebar work

--- general.js
+++ general.js
@@ -1,5 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("menu-button").addEventListener("click", {
-        
-    })
-})
+    let expanded = false;
+    const toggleSidebar = () => {
+        expanded = !expanded;
+        document.getElementsByTagName("nav")[0].className = expanded ? "expanded" : "";
+        document.getElementById("menu-button").className = expanded ? "menu-item hidden" : "menu-item";
+        document.getElementById("menu-expand-button").getElementsByTagName("img")[0].src = expanded ? "/icons/chevron-left.svg" : "/icons/chevron-right.svg"
+    };
+    document.getElementById("menu-expand-button").addEventListener("click", toggleSidebar);
+    document.getElementById("menu-button").addEventListener("click", toggleSidebar);
+});
